f22cdb08595b63eea50308101179681da1ff1c88
Sarah Greisdorf <sarah.greisdorf@gmail.com>
2020-02-04 22:26:51 -0500

    First pass at PSET 1 problems

--- Problem Sets/PS1/PS1.P2.js
+++ Problem Sets/PS1/PS1.P2.js
@@ -9,12 +9,35 @@
 *
 * NEED A SWITCH STATEMENT*/
 
-const evaluate = expr => eval(expr);
+
+const evaluate =  expr => {
+    if (expr.includes('+')) {
+        return (left, right) => {return left + right};
+    }
+    else if (expr.includes('-')) {
+        return (left, right) => {return left - right};
+    }
+    else if (expr.includes('*')) {
+        return (left, right) => {return left * right};
+    }
+    else {
+        return (left, right) => {return left / right};
+    }
+}
+
 const exprOne = '4+2';
+let calculate = evaluate(exprOne);
+console.log(`The expression ${exprOne} returns ${calculate(parseInt(exprOne.charAt(0)), parseInt(exprOne.charAt(2)))}`);
+
 const exprTwo = '5*7';
+calculate = evaluate(exprTwo);
+console.log(`The expression ${exprTwo} returns ${calculate(parseInt(exprTwo.charAt(0)), parseInt(exprTwo.charAt(2)))}`);
+
 const exprThree = '6-1';
+calculate = evaluate(exprThree);
+console.log(`The expression ${exprThree} returns ${calculate(parseInt(exprThree.charAt(0)), parseInt(exprThree.charAt(2)))}`);
+
 const exprFour = '9/2';
-console.log(`When we evaluate ${exprOne} we get ${evaluate(exprOne)}`);
-console.log(`When we evaluate ${exprTwo} we get ${evaluate(exprTwo)}`);
-console.log(`When we evaluate ${exprThree} we get ${evaluate(exprThree)}`);
-console.log(`When we evaluate ${exprFour} we get ${evaluate(exprFour)}`);
+calculate = evaluate(exprFour);
+console.log(`The expression ${exprFour} returns ${calculate(parseInt(exprFour.charAt(0)), parseInt(exprFour.charAt(2)))}`);
+
--- Problem Sets/PS1/PS1.P3.js
+++ Problem Sets/PS1/PS1.P3.js
@@ -1,36 +1,24 @@
-
-
-
-const evalFunc = (aString, execute) => execute(aString);
-
-
-/*Next, write two expressions that call this function. For the first, pass the string
-‘supercalifragilisticexpialidocious’ and a lambda function that returns an array containing
-fragments of the input string broken on the character ‘c’. For the input string
-‘supercalifragilisticexpialidocious’, you should get
-    [‘super’, ‘califragilisti’, ‘cexpialido’, ‘cious’] */
-
+function evalFunc (aString, execute) {
+    return execute(aString)
+}
 
 let result = evalFunc('supercalifragilisticexpialidocious',
     word => word.replace(/c/g, '*c').split('*')
 )
+
 console.log(result);
 
-/*For the second, pass the string ‘supercalifragilisticexpialidocious’ and a lambda function that
-replaces all of the ‘a’ characters with ‘A’ characters. Return an object that contains the original
-string, the modified string, the total number of As in the modified string, and the overall length
-of the modified string:*/
+let func2 = evalFunc( 'supercalifragilisticexpialidocious',word => {
 
-let func2 = (aString, func) => evalFunc( 'supercalifragilisticexpialidocious',word => word.replace(/a/g, 'A'));
-    let thing = {
-        originalString:  aString,
-        modifiedString: word,
-        numberReplaced: word.match('A').length(),
-        length: word.length()
+    let returnOb = {
+        originalString: word,
+        modifiedString: word.replace(/a/g, 'A'),
+        numberReplaced: word.split('a').length -1,
+        length: word.length
+    };
+
+    return returnOb
     }
-
-    return thing;
-
-}
+);
 
 console.log(func2)
